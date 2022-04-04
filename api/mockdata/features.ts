6c97c5ddf17f0548e1bf2f9cd8c62b7ad5f69b46
Nick Dimitrov <snoop088@gmail.com>
2022-04-04 16:38:38 +0300

    finalise features content. global adjustments to line-height and font-size rework the showcase items list

--- api/mockdata/features.ts
+++ api/mockdata/features.ts
@@ -34,7 +34,7 @@ export const FEATURES_SLIM: FeatureItemSlim[] = [
       "Export videos for your campaigns for social networks, YouTube or offline points of sale",
       "Engage audiences across channels, rapidly iterating new offer messages",
     ],
-    mediaUrl: "/768x576_637840758748332134.jpeg",
+    mediaUrl: "/videos.jpg",
   },
   {
     id: "Galleries",
@@ -42,42 +42,42 @@ export const FEATURES_SLIM: FeatureItemSlim[] = [
     title: "Galleries",
     benefits: [
       "Manage branded assets in reusable galleries",
-      "Enable asset sharing across different roduction pipelines",
+      "Enable asset sharing across different production pipelines",
       "Ensure that graphics in ads are relevant, on brand and readily available to marketing",
     ],
     mediaUrl: "/768x576_637840758748332134.jpeg",
   },
 ];
 export const FIND_OUT_MORE =
-  "FIND OUT MORE ABOUT OUR FREE ENTERPRISE FEATURES THAT ACCELERATE YOUR AD PODUCTION";
+  "FIND OUT MORE ABOUT OUR FREE ENTERPRISE FEATURES THAT ACCELERATE YOUR AD PODUCTION.";
 export const HELPING = {
   title: "LET US HELP YOU GET STARTED",
-  copy: "Contact our digital experts for a quick demo to see Streameye in action",
+  copy: "Contact our digital experts for a quick demo to see Streameye in action.",
 };
 export const ENTERPRISE_FEATURES: FeatureEnterprise[] = [
   {
     title: "SSO",
     copyMd:
       "Single sign on boosts security and enables you to control access to the platform via your own infrastructure.",
-    contract: "Free with 1 year contract",
+    contract: "Free with 1 year contract.",
   },
   {
     title: "APPROVALS",
     copyMd:
       "Approvals feature enforces your teams to publish only approved content by another designated party in your organisation. It is especially helpful for regulated markets.",
-    contract: "Free with any contract",
+    contract: "Free with any contract.",
   },
   {
     title: "CAMPAIGNS",
     copyMd:
       "Grouping ads in campaigns allows you to swap in new ads with different designs in almost real time. This is useful to publish completely new banners in your currently trafficked placements.",
-    contract: "Free with any contract",
+    contract: "Free with any contract.",
   },
   {
     title: "LOCALISATION",
     copyMd:
       "Single entry point localisation allows you to localise many languages simultaneously through a centralised spreadsheet. An operator can opt to enter the localisations or send them to be localised by other parties.",
-    contract: "Free with any contract",
+    contract: "Free with any contract.",
   },
   {
     title: "API INTEGRATION",
@@ -92,23 +92,23 @@ export const FEATURES_FULL: FeatureItem[] = [
     title: "Templates",
     icon: "templates",
     descriptionMd:
-      "Streameye templates are a core feature, which optimises the production of animated ads, keeping their production cost negligent. It is used by our partners and clients to generate ads en masse and deploy relevant promotions across wide array of ad servers with a click of a button.",
+      "Streameye templates are a core feature, which optimises the production of animated ads, keeping their production cost minimal. It is used by our partners and clients to generate ads en masse and deploy relevant promotions across wide array of ad servers with a click of a button.",
     usingFor: {
       title: "USE TEMPLATES TO:",
       uses: [
-        "Produce engaging HTML5 animated ads with your latest promotions",
-        "Go to market in a matter of minutes and update your display ads without the need to republish them",
-        "Automate your ad production by integrating your data in the ads",
-        "Save time and free up resources by streamlining your production processes",
-        "Decrease production costs and increase ROI",
+        "Produce engaging HTML5 animated ads with your latest promotions.",
+        "Go to market in a matter of minutes and update your display ads without the need to republish them.",
+        "Automate your ad production by integrating your data in the ads.",
+        "Save time and free up resources by streamlining your production processes.",
+        "Decrease production costs and increase ROI.",
       ],
     },
     gettingStarted: {
       title: "GET STARTED IN FEW SIMPLE STEPS:",
       steps: [
-        "Decide where you experience production bottlenecks",
-        "Discuss your priorities with our experienced digital team",
-        "Take advantage of a 1 month free trial with your own branded ads",
+        "Decide where you experience production bottlenecks.",
+        "Discuss your priorities with our experienced digital team.",
+        "Take advantage of a 1 month free trial with your own branded ads.",
       ],
     },
     mediaUrl: "templates.mp4",
@@ -123,18 +123,70 @@ export const FEATURES_FULL: FeatureItem[] = [
       title: "USE BUILDERS TO:",
       uses: [
         "Interactively produce static images through a custom UI built for your specific brand.",
-        "Export images for social networks, emails, landing pages, your site or affiliates",
-        "Automate your ad production by integrating your data in the ads by using permalinks",
-        "Save time and free up resources by streamlining your production processes",
-        "Make your marketing self provisioned when deploying new offers live",
+        "Export images for social networks, emails, landing pages, your site or affiliates.",
+        "Automate your ad production by integrating your data in the ads by using permalinks.",
+        "Save time and free up resources by streamlining your production processes.",
+        "Make your marketing self provisioned when deploying new offers live.",
       ],
     },
     gettingStarted: {
       title: "GET STARTED IN FEW SIMPLE STEPS:",
       steps: [
-        "Decide where you experience production bottlenecks",
-        "Discuss your priorities with our experienced digital team",
-        "Take advantage of a 1 month free trial with your own branded ads",
+        "Decide where you experience production bottlenecks.",
+        "Discuss your priorities with our experienced digital team.",
+        "Take advantage of a 1 month free trial with your own branded ads.",
+      ],
+    },
+    mediaUrl: "builders.mp4",
+  },
+  {
+    id: "Videos",
+    title: "Videos",
+    icon: "videos",
+    descriptionMd:
+      "Streameye videos are a core feature, which enables you to export videos to any social channel or offline media. Ability to rapidly generate videos that engage your audiences across channels would give you a definite edge over your competition. Alleviate your creative resource, allowing your creative team to design bespoke content, while using Streameye to generate your recurring offers.",
+    usingFor: {
+      title: "USE VIDEOS TO:",
+      uses: [
+        "Quickly produce videos for various online and offline channels.",
+        "Engage your audience with captivating, fluid motion.",
+        "Export videos in a matter of minutes without animation skills.",
+        "Empower your marketing team with video ads without blocking motion team resource.",
+        "Get an edge over your competitors with always relevant content.",
+      ],
+    },
+    gettingStarted: {
+      title: "GET STARTED IN FEW SIMPLE STEPS:",
+      steps: [
+        "Decide which videos need producing on regular basis.",
+        "Discuss your priorities with our experienced digital team.",
+        "Take advantage of a 1 month free trial with your own branded ads.",
+      ],
+    },
+    mediaUrl: "videos.mp4",
+  },
+  {
+    id: "Galleries",
+    title: "Asset Galleries",
+    icon: "asset_galleries",
+    descriptionMd:
+      "Streameye asset galleries are a supporting feature, which holds your branded assets and facilitates their easy sharing between your templates, videos and builders. Your creative team is able to maintain this resource bank, ensuring that only the latest and relevant assets are available to your marketing.",
+    usingFor: {
+      title: "USE GALLERIES TO:",
+      uses: [
+        "Create a resource bank that can be easily reused across digital ads.",
+        "Feed and update your ads with relevant imagery.",
+        "Enable your inhouse studio or external agency to support your ad production efficiently.",
+        "Ensure your ads only use approved and on-brand imagery.",
+        "Connect galleries to APIs to automate image selection in ads.",
+      ],
+    },
+    gettingStarted: {
+      title: "GET STARTED IN FEW SIMPLE STEPS:",
+      steps: [
+        "Decide which templates or builders need to use graphic assets.",
+        "Get expert recommendation how to streamline your assets production from our experienced team.",
+        "Take advantage of a 1 month free trial with your own branded ads.",
       ],
     },
     mediaUrl: "builders.mp4",
