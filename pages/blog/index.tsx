7207118ae0921eaf09066361c71e5cd6cef29b53
Nick Dimitrov <snoop088@gmail.com>
2022-04-10 14:19:41 +0300

    loader for infinite scroll added

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,12 +1,12 @@
 import { BlogSlim } from "@/components/molecules/blog-slim/blog-slim";
 import Wrapper from "@/components/organisms/Wrapper/Wrapper";
 import { BLOG_POSTS_SLIM } from "api/mockdata/blog";
-import { sensitiveHeaders } from "http2";
 import { BlogItemSlim } from "interfaces/blog-item-slim";
 import { PageWithMeta } from "interfaces/page-with-meta";
 import { NextPage } from "next";
 import { useMemo, useState } from "react";
 import InfiniteScroll from "react-infinite-scroller";
+import Image from "next/image";
 import styles from "./index.module.scss";
 
 interface PageProps extends PageWithMeta {
@@ -25,24 +25,19 @@ const Index: NextPage<PageProps> = ({ blogPostsSlim }: PageProps) => {
     let i = 0;
     for (let i = 0; i < num; i++) {
       const a: BlogItemSlim[] = blogPostsSlim.map((post) => ({
-        id: post.id + "_" + i,
-        medialUrl: post.medialUrl,
-        author: post.author,
-        date: post.date,
-        title: post.title,
-        shortCopy: post.shortCopy,
+        ...post,
+        id: post.id + i,
       }));
-      const p = new Promise((resolve, reject) =>
-        setTimeout(() => resolve(null), timeInterval)
-      );
+      const p = new Promise((resolve) => setTimeout(resolve, timeInterval));
       await p;
       yield a;
     }
   }
   // must memoise or it will be recreated on re-render!
+  // no dependencies needed as this is only derived once
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const nextPostsCall = useMemo(() => articleGenerator(3, 1000), []);
   const blogLoader = async () => {
-    console.log("well then?");
     const loaded = await nextPostsCall.next();
     if (loaded.done) {
       setHasNext(false);
@@ -56,7 +51,17 @@ const Index: NextPage<PageProps> = ({ blogPostsSlim }: PageProps) => {
         pageStart={1}
         loadMore={blogLoader}
         hasMore={hasNext}
-        loader={<div key={873432}>I am loading!</div>}
+        loader={
+          <div key={873432} className={styles.loader}>
+            <Image
+              className={styles.loadingAd}
+              src="/loader-puff-dark.svg"
+              width={32}
+              height={32}
+              alt="Loading"
+            />
+          </div>
+        }
       >
         <Wrapper isWide className={styles.blogGrid}>
           {posts.map((post) => (
