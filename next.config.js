26e6b708b6936c6def938d42d8bf7afe3a73e8da
Nick Dimitrov <snoop088@gmail.com>
2022-04-21 16:21:51 +0300

    adding image domains for nextjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "streameye.com",
+      "streameye.app",
+      "streameye.bg",
+      "www.streameye.com",
+      "www.streameye.app",
+      "www.streameye.bg",
+    ],
+  },
+};
