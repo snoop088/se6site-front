9c63f806bae2f22915d08f2a0c13a873625d84f5
Ivailo Velkov <ivailo.velkov.ext@spotlightsportsgroup.com>
2022-04-21 14:27:10 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="google-analytics">
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
