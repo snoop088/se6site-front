5c39697832dda7cb646b10608255de8d3228794c
Ivailo Velkov <ivailo.velkov.ext@spotlightsportsgroup.com>
2022-04-26 13:48:50 +0300

    upd

--- components/molecules/consent/consent.tsx
+++ components/molecules/consent/consent.tsx
@@ -1,27 +1,57 @@
-import Link from "next/link";
+import Script from "next/script";
 import React from "react";
-// import CookieConsent from "react-cookie-consent";
+
+declare global {
+  interface Window {
+    initCookieConsent: any;
+  }
+}
 
 export const Consent = () => {
   return (
-    <p>consent</p>
-    // <CookieConsent
-    //   buttonText="Yes, I accept cookies"
-    //   style={{ alignItems: "center", lineHeight: 1.2 }}
-    //   buttonStyle={{
-    //     background: "#56bd52",
-    //     color: "#fff",
-    //     fontSize: "14px",
-    //   }}
-    // >
-    //   <span style={{ fontSize: "14px" }}>
-    //     This website uses cookies to ensure you get the best experience on our
-    //     website. Read our{" "}
-    //     <Link href={"/cookie-policy"}>
-    //       <a style={{ textDecoration: "underline" }}>cookie policy</a>
-    //     </Link>
-    //   </span>
-    //   .
-    // </CookieConsent>
+    <Script
+      src="/libs/cookieconsent.js"
+      onLoad={() => {
+        const cc = window.initCookieConsent();
+
+        cc.run({
+          current_lang: "en",
+          gui_options: {
+            consent_modal: {
+              layout: "cloud",
+              position: "bottom center",
+              transition: "slide",
+              swap_buttons: false,
+            },
+            settings_modal: {
+              layout: "box",
+              transition: "slide",
+            },
+          },
+          languages: {
+            en: {
+              consent_modal: {
+                title: null,
+                description:
+                  'This website uses cookies to ensure you get the best experience on our website. Read our <a href="/cookie-policy">cookie policy</a>.',
+                primary_btn: {
+                  text: "Yes, I accept cookies",
+                  role: "accept_all",
+                },
+              },
+              settings_modal: {
+                title: "cookie title",
+                save_settings_btn: "Save settings",
+                accept_all_btn: "Accept all",
+                reject_all_btn: "Reject all",
+                close_btn_label: "Close",
+                cookie_table_headers: [],
+                blocks: [],
+              },
+            },
+          },
+        });
+      }}
+    />
   );
 };
